282338424f7d155778928a887d0d3c819b91e38a
YogeshVenugopalR <yogesh.r@zipsar.com>
2025-08-11 06:33:36 +0300

    Route setup and package installation and font declaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Career from "./Pages/Career";
+import Club from "./Pages/Club";
+import Contact from "./Pages/Contact";
+import Programme from "./Pages/Pragramme";
+import Research from "./Pages/Research";
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/programme" element={<Programme />} />
+        <Route path="/club" element={<Club />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/contact-us" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/Pages/Pragramme.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pragramme = () => {
+  return <div></div>;
+};
+
+export default Pragramme;
